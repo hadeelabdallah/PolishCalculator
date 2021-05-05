c8fb1fb0b2b2e2e59ec0a03ec3138ae239483972
Hadeel Abdallah <hadeel@Hadeels-MacBook-Pro.local>
2021-05-05 11:37:00 -0500

    GREEN - again fixed failing test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,19 +1,25 @@
 import java.math.BigDecimal;
+import java.util.Stack;
 
 public class Calculator {
-    private BigDecimal accumulator = BigDecimal.ZERO;
+    private Stack<BigDecimal> stack = new Stack();
 
     public BigDecimal getAccumulator() {
-        return accumulator;
+        return stack.size() != 0 ? stack.peek() : BigDecimal.ZERO;
     }
 
     public void setAccumulator(BigDecimal bigDecimal) {
-        accumulator = bigDecimal;
+        if(stack.size() > 0){
+            stack.pop();
+        }
+        stack.push(bigDecimal);
     }
 
     public void enter() {
+        stack.push(getAccumulator());
     }
 
     public void drop() {
+        stack.pop();
     }
 }
